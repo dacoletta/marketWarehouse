a9177e19e17883a7b212d3f281605f8d250d976e
Danilo Antonio Coletta <dacoletta.95@gmail.com>
2024-05-11 19:13:21 +0200

    Add user: main fixes

--- constants/constants.js
+++ constants/constants.js
@@ -10,15 +10,20 @@ export const CONSTANTS = {
     CONFIRM: "Confirm",
     CLEAR: "Clear",
     PASSWORD: "Password",
+    REPEAT_PASSWORD: "Repeat password",
     LOGIN: "Login",
     USE_DEFAULT_VALUES: "Use default values",
     RESET: "Reset",
     NEW_USER: "New user?",
-
+    LASTNAME: "Lastname",
+    FIRSTNAME: "Firstname",
+    USERNAME: "Username",
+    BACK: "Back",
     MSG_CONFIRM_OPERATION: "Confirm operation",
     MSG_CONFIRM_DELETE_TITLE: "Confirm delete",
     MSG_CONFIRM_DELETE: "Are you sure you want to delete the selected product?",
     MSG_CONFIRM_FORM_TEXT: "Are you sure you want to confirm the entered data?",
     MSG_INVALID_CREDENTIALS_TITLE: "Login failed",
-    MSG_INVALID_CREDENTIALS_TEXT: "Invalid credentials"
+    MSG_INVALID_CREDENTIALS_TEXT: "Invalid credentials",
+    
 }
--- pages/addUser.vue
+++ pages/addUser.vue
@@ -1,34 +1,35 @@
 <template>
     <div>
-        <h1 class="title-page">ADD USER</h1>
         <form @submit.prevent="submit">
             <v-row class="justify-center align-center">
                 <v-col cols="12" md="5">
                     <v-text-field v-model="lastName.value.value" :error-messages="lastName.errorMessage.value"
-                        label="Lastname"></v-text-field>
+                        :label="C.LASTNAME"></v-text-field>
 
 
                     <v-text-field v-model="firstName.value.value" :error-messages="firstName.errorMessage.value"
-                        label="Firstname"></v-text-field>
+                        :label="C.FIRSTNAME"></v-text-field>
                     <v-radio-group inline>
                         <v-radio label="M" value="M" v-model="gender.value.value"></v-radio>
                         <v-radio label="F" value="F" v-model="gender.value.value"></v-radio>
                     </v-radio-group>
                     <v-text-field v-model="username.value.value" :error-messages="username.errorMessage.value"
-                        label="Username"></v-text-field>
-                    <v-text-field v-model="password.value.value" :error-messages="password.errorMessage.value"
-                        label="Password"></v-text-field>
+                        :label="C.USERNAME"></v-text-field>
+                    <v-text-field :type="showPwd ? 'text' : 'password'" :append-inner-icon="showPwd ? 'mdi-eye' : 'mdi-eye-off'" @click:append-inner="showPwd = !showPwd" v-model="password.value.value" :error-messages="password.errorMessage.value"
+                        :label="C.PASSWORD"></v-text-field>
+                    <v-text-field :type="showRepeatPwd ? 'text' : 'password'" :append-inner-icon="showRepeatPwd ? 'mdi-eye' : 'mdi-eye-off'" @click:append-inner="showRepeatPwd = !showRepeatPwd" v-model="repeatPassword.value.value" :error-messages="repeatPassword.errorMessage.value"
+                        :label="C.REPEAT_PASSWORD"></v-text-field>
 
-                    <div style="text-align: right;">
-                        
+                    <div id="buttons-container">
+
                         <v-btn class="me-4" color="error" @click="router.push('/login');">
-                            Back
+                            {{ C.BACK }}
                         </v-btn>
                         <v-btn class="me-4" @click="handleReset">
-                            Reset
+                            {{ C.RESET }}
                         </v-btn>
-                        <v-btn  color="success" type="submit">
-                            Confirm
+                        <v-btn color="success" type="submit" :disabled="isDisabled">
+                            {{ C.CONFIRM }}
                         </v-btn>
                     </div>
 
@@ -40,7 +41,8 @@
 </template>
 
 <script setup lang="ts">
-import { useField, useForm } from 'vee-validate';
+import { CONSTANTS as C } from '~/constants/constants';
+import { useField, useForm, useIsFormValid } from 'vee-validate';
 import { useAuthStore } from '~/store/authStore';
 const router = useRouter();
 const authStore = await useAuthStore();
@@ -61,19 +63,31 @@ const { handleSubmit, handleReset } = useForm({
 
             return 'Username needs to be at least 2 characters.'
         },
+        password(value: string | any[]) {
+            if (value?.length >= 8) return true
+
+            return 'Password needs to be at least 8 characters.'
+        },
+        repeatPassword(value: string | any[]) {
+            if (value === password.value.value) return true
+
+            return 'Passwords doesn t match.'
+        },
     },
 })
-
+const isValid = useIsFormValid();
+const isDisabled = computed(() => {
+    return !isValid.value;
+});
+let showPwd = ref(false);
+let showRepeatPwd = ref(false);
 const firstName = useField('firstName');
 const lastName = useField('lastName');
 const gender = useField('gender');
 const username = useField('username');
 const password = useField('password');
-
-const setInitialValues = async () => {
-}
-
-setInitialValues();
+const repeatPassword = useField('repeatPassword');
+gender.value.value = "F"
 const submit = handleSubmit(async (values: any) => {
     // Salvo i dati in un oggetto
     const obj = { firstName: values.firstName, lastName: values.lastName, gender: values.gender, username: values.username, password: values.password };
@@ -87,11 +101,16 @@ const submit = handleSubmit(async (values: any) => {
         .then(res => res.json())
         .then(res => newUser = res);
     // Salvo nello state e in localstorage
-    await authStore.addUser(newUser);
+    authStore.addUser(newUser);
     localStorage.setItem('users', JSON.stringify(authStore.users));
     router.push('/');
 
 })
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#buttons-container {
+    display: flex;
+    justify-content: end;
+}</style>
